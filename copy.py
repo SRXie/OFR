45ec1b9186f8f7902f3c445deb34f3d93a8d00ce
SRXie <sirayshei@gmail.com>
2022-01-06 15:15:02 -0800

    ...

--- copy.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for dirt in ["images"]:
-    for index in ['5']:
-        source = "/checkpoint/siruixie/clevr_obj_test/output/"+index+"/obj_test/"+dirt+"/."
-        dest = "/checkpoint/siruixie/clevr_obj_test/output/obj_test_occ/"+dirt
-        os.system("cp -a "+source+" "+dest)
-        print(dirt, index)
